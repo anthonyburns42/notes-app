1b721c0695ec6cd97d0bfcc97bd55bcdf9ccdd25
Anthony Burns <aburns42@gmail.com>
2022-04-09 16:04:21 -0400

    Added remove note functionality to Note App and meaningful console logs to the user about what is happening.

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function() {
     return 'Your notes...';
@@ -18,11 +19,25 @@ const addNote = function (title, body) {
         });
 
         saveNotes(notes);
-        console.log('New note added!');
+        console.log(chalk.green.inverse('New note added!'));
     } else {
-        console.log('Note title taken!');
+        console.log(chalk.red.inverse('Note title taken!'));
     }
+}
 
+const removeNote = function (title) {
+    const notes = loadNotes();
+
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title;
+    });
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Notes removed!'));
+        saveNotes(notesToKeep);
+    } else {
+        console.log(chalk.red.inverse('No note found.'));
+    }
 }
 
 const saveNotes = function (notes) {
@@ -43,5 +58,6 @@ const loadNotes = function () {
 
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote
 };
